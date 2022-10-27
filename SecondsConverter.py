53341c8f2f77f6d83b0c804a7bf5a3018c0f77de
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-10-27 15:17:12 -0300

    Add files via upload

---
+++ SecondsConverter.py
@@ -0,0 +1,6 @@
+days = int(input("Days: "))
+hours = int(input("Hours: "))
+minutes = int(input("Minutess: "))
+
+total = days * 24 * 3600 + hours * 3600 + minutes * 60
+print("Converted to seconds is equal to %10d seconds." %total)
