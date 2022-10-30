b5539225d6977a989b76ba1532728232cc786c9d
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-10-30 17:00:13 -0300

    Update SecondsConverter.py

--- SecondsConverter.py
+++ SecondsConverter.py
@@ -1,9 +1,6 @@
 # Program that receives a number of days, hours and minutes
 # and converts the entered values ​​to seconds
 
-days = int(input("Days: "))
-hours = int(input("Hours: "))
-minutes = int(input("Minutess: "))
 
 total = days * 24 * 3600 + hours * 3600 + minutes * 60
 print("Converted to seconds is equal to %10d seconds." %total)
