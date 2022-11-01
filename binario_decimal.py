b3e79a6eec842e74196ef2c74441a0686a671882
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-11-01 13:19:58 -0300

    Update binario_decimal.py

--- binario_decimal.py
+++ binario_decimal.py
@@ -1,4 +1,5 @@
 # Programa que recebe um número binário digitado pelo usuário 
+# Depois transforma em um número decimal
 
 binario = int(input("Digite o número (binário) para ser convertido para a base decimal: "))
 n = len(str(binario))
