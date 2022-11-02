d28c9b64796f1d4c63b535996b31d2fee843847f
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-11-02 17:16:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+# Você e seu time estão desenvolvendo um sistema de indicações de postos de gasolina que ficam próximos da localização atual do veículo.
+# No modo de direção “viagem”, a funcionalidade a ser desenvolvida é de indicar ao condutor o posto mais distante possível dentro do limite atual de combustível.
+# E caso não exista posto de gasolina, retornar -1
+
+# A pessoa responsável por fazer a especificação do sistema informou que você terá as seguintes informações: consumo médio de combustível, 
+# quantidade de combustível restante no veículo e um array contendo distâncias dos postos de gasolinas.
+
+# Exemplo:
+# Combustivel (em litros): 2
+# Consumo médio (km/l): 8
+# Postos de Gasolina (km): [2, 15, 22, 10.2]
+
+
 def ultima_parada(combustivel,consumo,postos_de_gasolina):
     pass
     
