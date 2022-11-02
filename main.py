268dd74700458e6fa12bf7f102712640af075e54
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-11-02 17:17:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,9 @@
 # Consumo médio (km/l): 8
 # Postos de Gasolina (km): [2, 15, 22, 10.2]
 
-
+def ultima_parada(combustivel,consumo,postos_de_gasolina):
+    pass
+    
 
     postos_de_gasolina = [2,10.2,15,22]
     alcance = (combustivel * consumo) - 1
