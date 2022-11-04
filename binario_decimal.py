a54e1fa51a7865f00cf139012981391147dab8ae
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-11-04 12:52:48 -0300

    Update binario_decimal.py

--- binario_decimal.py
+++ binario_decimal.py
@@ -1,19 +1,27 @@
 # Código que recebe um valor binário, depois transforma-o em uma valor decimal
 
-binario = 1011010100 
+def calcula_numero_da_senha(senha):
+    pass
 
-n = len(str(binario))
-valor_digitado = binario
-decimal = 0
-i = 0
+    senha = 1011010100 
 
-while n >= 0:
-    resto = binario % 10
-    decimal = decimal + (resto * (2**i))
-    n = n - 1
-    i = i + 1
-    binario = binario // 10
+    n = len(str(senha))
+    valor_digitado = senha
+    decimal = 0
+    i = 0
+
+    while n >= 0:
+  
+        resto = senha % 10
+        decimal = decimal + (resto * (2**i))
+        n = n - 1
+        i = i + 1
+        senha = senha // 10
     
-print(decimal)
+    return decimal 
+    
+print(calcula_numero_da_senha(1011010100))
+
+
 
 
