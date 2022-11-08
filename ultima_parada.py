f704925fc6a3e1b57568bf63097380cae6183f6f
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-11-08 10:11:00 -0300

    Add files via upload

---
+++ ultima_parada.py
@@ -0,0 +1,15 @@
+def ultima_parada(combustivel,consumo,postos_de_gasolina):
+    pass
+    
+    alcance = combustivel * consumo
+    postos_de_gasolina.sort()
+    postos_de_gasolina.reverse()
+    i=0
+
+    if postos_de_gasolina:
+      for posto in postos_de_gasolina:
+        if alcance >= posto:
+            return posto
+      return -1 
+
+print(ultima_parada(2,8,[9,11,15,22]))
