3dd32190061afb16e3186669565b085e1e2c598c
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-11-13 19:11:01 -0300

    Update preço_aluguel_carrro.py

--- preço_aluguel_carrro.py
+++ preço_aluguel_carrro.py
@@ -1,10 +1,1 @@
-# Programa que calcula o valor a ser pago pelo cliente
-# Baseando-se nos km pecorridos e no número de dias que ele foi utilizado
 
-
-
-
-km_pecorrido = float(input("Informe os km percorridos: "))
-dias_alugados = int(input("Informe quantos dias ele foi alugado: "))
-preco = (60*dias_alugados) + (0.15 * km_pecorrido)
-print("O valor a pagar foi de: R$",preco)
