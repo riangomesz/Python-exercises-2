4d6bee804bed35cad8be795bb2ed05e72b0a8678
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-12-02 11:59:40 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,28 @@
+# Função tudo que retorna valor
+# Metodo é aquilo que não retorna
+
+class Calculadora:
+    def __init__(self, num1, num2):
+        self.valor_a = num1
+        self.valor_b = num2
+
+    def soma(self):
+        return self.valor_a + self.valor_b
+
+    def subtracao(self):
+        return self.valor_a - self.valor_b
+
+    def multi(self):
+        return self.valor_a * self.valor_b
+
+    def divisao(self):
+        return self.valor_a / self.valor_b
+
+    def potencia(self):
+        return self.valor_a ** self.valor_b
+    
+calculadora = Calculadora(20,2)
+print(calculadora.soma())
+print(calculadora.subtracao())
+print(calculadora.divisao())
+print(calculadora.multi())
